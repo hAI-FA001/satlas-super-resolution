3e9f2ec6795b174d07c9a8412d1ebb3902b0e889
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-07-11 11:16:18 +0500

    add CPU option

--- ssr/metrics/lpips.py
+++ ssr/metrics/lpips.py
@@ -3,19 +3,24 @@ import torch
 
 from basicsr.utils.registry import METRIC_REGISTRY
 
+from ssr.override_device import USE_CPU
+
+
 @METRIC_REGISTRY.register()
 def calculate_lpips(img, img2, lpips_model, **kwargs):
-    device = torch.device('cuda')
+    device = torch.device("cuda" if not USE_CPU else "cpu")
 
-    if lpips_model == 'alexnet':
-        lpips_loss_fn = lpips.LPIPS(net='alex').to(device) # best forward scores
-    elif lpips_model == 'vgg':
-        lpips_loss_fn = lpips.LPIPS(net='vgg').to(device) # closer to "traditional" perceptual loss, when used for optimization
+    if lpips_model == "alexnet":
+        lpips_loss_fn = lpips.LPIPS(net="alex").to(device)  # best forward scores
+    elif lpips_model == "vgg":
+        lpips_loss_fn = lpips.LPIPS(net="vgg").to(
+            device
+        )  # closer to "traditional" perceptual loss, when used for optimization
 
     tensor1 = torch.as_tensor(img).permute(2, 0, 1)
-    tensor1 = tensor1.unsqueeze(0).to(device).float()/255
+    tensor1 = tensor1.unsqueeze(0).to(device).float() / 255
     tensor2 = torch.as_tensor(img2).permute(2, 0, 1)
-    tensor2 = tensor2.unsqueeze(0).to(device).float()/255
+    tensor2 = tensor2.unsqueeze(0).to(device).float() / 255
 
     lpips_loss = lpips_loss_fn(tensor1, tensor2).detach().item()
     return lpips_loss
